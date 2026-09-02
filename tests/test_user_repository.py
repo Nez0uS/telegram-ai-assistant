3147296b28fd39eb8a16836c149867918f289cbf
Nez0uS <sergei.sam2110@gmail.com>
2026-09-02 22:18:56 +0400

    test - add user deletion test

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -30,3 +30,21 @@ async def test_get_user_returns_none_if_user_does_not_exist(user_repository):
 
     finally:
         await user_repository.delete_user(telegram_id)
+
+
+@pytest.mark.anyio
+async def test_delete_user(user_repository):
+    telegram_id = 12345
+    name = "name"
+
+    await user_repository.delete_user(telegram_id)
+
+    try:
+        await user_repository.create_user(telegram_id, name)
+        await user_repository.delete_user(telegram_id)
+
+        user = await user_repository.get_user(telegram_id)
+
+        assert user is None
+    finally:
+        await user_repository.delete_user(telegram_id)
